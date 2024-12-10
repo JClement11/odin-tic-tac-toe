31d9da922efb3c3e1b8e8766559fc393476beb99
JClement11 <jclement0829@gmail.com>
2024-12-10 17:39:02 -0600

    Create function to check win/tie

--- index.js
+++ index.js
@@ -16,6 +16,33 @@ const GameController = (function () {
     function changeTurn() {
         currentPlayer = (currentPlayer === playerOneMarker) ? playerTwoMarker : playerOneMarker;
     }
-    return { changeTurn };
+
+    function checkWin(board, player) {
+
+        for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+            if (board[rowIndex][0] === player && board[rowIndex][1] === player && board[rowIndex][2] === player) {
+                return console.log("win");
+            }
+        }
+        for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
+            if (board[0][columnIndex] === player && board[1][columnIndex] === player && board[2][columnIndex] === player) {
+                return console.log("win");
+            }
+        }
+        if (board[0][0] === player && board[1][1] === player && board[2][2] === player ||
+            board[0][2] === player && board[1][1] === player && board[2][0] === player) {
+            return console.log("win");
+        }
+        for (let row = 0; row < 3; row++) {
+            for (let column = 0; column < 3; column++) {
+                if (board[row][column] === "") {
+                    return false;
+                }
+            }
+        }
+        return console.log("tie");
+    }
+
+    return { changeTurn, checkWin };
 })();
 
